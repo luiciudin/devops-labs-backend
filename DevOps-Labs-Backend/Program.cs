d806d08ee2c5bb9b42c8191683f5406a50ce4b4f
Ubuntu <luiza.ciudin@yahoo.com>
2023-04-08 16:28:07 +0000

    Changing port in Program.cs

--- DevOps-Labs-Backend/Program.cs
+++ DevOps-Labs-Backend/Program.cs
@@ -23,7 +23,6 @@ if (!builder.Environment.IsEnvironment("Local"))
     builder.WebHost.UseKestrel(configure =>
     {
         configure.Listen(System.Net.IPAddress.Any, 8080);
-	configure.Listen(System.Net.IPAddress.Any, 4000);
         configure.Listen(System.Net.IPAddress.Any, 80);
     });
 }
